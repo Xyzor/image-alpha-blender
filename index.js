c8ac41e97a9e73837fa3577fa646338356d93609
Xyzor <ricsibi23@gmail.com>
2024-10-05 21:36:16 +0200

    setup basic blending

--- index.js
+++ index.js
@@ -1,69 +1,15 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const fileInput = document.getElementById('fileInput');
-const previewArea = document.getElementById('previewArea');
+const fgAlphaInput = document.getElementById('fgAlpha');
 const uploadStatusArea = document.getElementById('uploadStatusArea');
+const previewArea = document.getElementById('previewArea');
 const reader = new FileReader();
 const imagesInPixel = [];
 
-const renderCanvas = () => {
-    const imageElements = document.getElementsByClassName('previewImg');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < imageElements.length; i++) {
-        if (!i) {
-            canvas.width = imageElements[i].naturalWidth;
-            canvas.height = imageElements[i].naturalHeight;
-        }
-        
-        context.drawImage(imageElements[i], 0, 0);
-    }
-};
-
-const getPixelDataFromImageElement = (image) => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    console.log('Getting ImageData for', image);
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-
-    context.drawImage(image, 0, 0);
-
-    return context.getImageData(0, 0, canvas.width, canvas.height);
+const getBlendedColorValue = (fgColor, fgAlpha, bgColor, bgAlpha, combinedAlpha) => {
+    return (fgAlpha * fgColor + ((1 - fgAlpha) * bgAlpha * bgColor)) / combinedAlpha;
 }
-
-const handleFileReaderEvent = (event) => {
-    uploadStatusArea.innerHTML += `<span>${event.type}: ${event.loaded} bytes transferred</span><br/>`;
-
-    if (event.type === "load") {
-        const imageElement = document.createElement('img');
-        imageElement.src = reader.result;
-        imageElement.classList = 'previewImg';
-        imageElement.title = fileInput.files[0].name;
-        imageElement.onload = () => {
-            previewArea.append(imageElement);
-            imagesInPixel.push(getPixelDataFromImageElement(imageElement));
-            renderCanvas();
-        };
-    }
-}
-
-fileInput.addEventListener('change', () => {
-    const selectedFile = fileInput.files[0];
-
-	if (selectedFile) {
-        uploadStatusArea.innerHTML += `<b>${selectedFile.name}</b><hr/>`;
-
-        reader.addEventListener("loadstart", handleFileReaderEvent);
-        reader.addEventListener("load", handleFileReaderEvent);
-        reader.addEventListener("loadend", handleFileReaderEvent);
-        reader.addEventListener("progress", handleFileReaderEvent);
-        reader.addEventListener("error", handleFileReaderEvent);
-        reader.addEventListener("abort", handleFileReaderEvent);
-
-        reader.readAsDataURL(selectedFile);
-	}
-});
 
 const composite = (bgImg, fgImg, fgOpac) => {
     if (fgOpac === 0) return bgImg;
@@ -95,6 +41,60 @@ const composite = (bgImg, fgImg, fgOpac) => {
     }
 }
 
-const getBlendedColorValue = (fgColor, fgAlpha, bgColor, bgAlpha, combinedAlpha) => {
-    return (fgAlpha * fgColor + ((1 - fgAlpha) * bgAlpha * bgColor)) / combinedAlpha;
+const renderCanvas = () => {
+    const imageElements = document.getElementsByClassName('previewImg');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    canvas.width = imageElements[0].naturalWidth;
+    canvas.height = imageElements[0].naturalHeight;
+
+    for (let i = imageElements.length - 1; i > 0; i--) {
+        composite(imagesInPixel[i - 1], imagesInPixel[i], Number(fgAlphaInput.value) / 100);
+    }
+    context.putImageData(imagesInPixel[0], 0, 0);
+};
+
+const getPixelDataFromImageElement = (image) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    console.log('Getting ImageData for', image);
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+
+    context.drawImage(image, 0, 0);
+
+    return context.getImageData(0, 0, canvas.width, canvas.height);
 }
+
+const handleFileReaderEvent = (event) => {
+    uploadStatusArea.innerHTML += `<span>${event.type}: ${event.loaded} bytes transferred</span><br/>`;
+
+    if (event.type === "load") {
+        const imageElement = document.createElement('img');
+        imageElement.src = reader.result;
+        imageElement.classList = 'previewImg';
+        imageElement.title = fileInput.files[0].name;
+        imageElement.onload = () => {
+            previewArea.append(imageElement);
+            imagesInPixel.push(getPixelDataFromImageElement(imageElement));
+            imagesInPixel.length > 1 && renderCanvas();
+        };
+    }
+}
+
+fileInput.addEventListener('change', () => {
+    const selectedFile = fileInput.files[0];
+
+	if (selectedFile) {
+        uploadStatusArea.innerHTML += `<b>${selectedFile.name}</b><hr/>`;
+
+        reader.addEventListener("loadstart", handleFileReaderEvent);
+        reader.addEventListener("load", handleFileReaderEvent);
+        reader.addEventListener("loadend", handleFileReaderEvent);
+        reader.addEventListener("progress", handleFileReaderEvent);
+        reader.addEventListener("error", handleFileReaderEvent);
+        reader.addEventListener("abort", handleFileReaderEvent);
+
+        reader.readAsDataURL(selectedFile);
+	}
+});
