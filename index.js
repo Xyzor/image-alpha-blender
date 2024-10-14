6c040c18d69f5ff43abc231b6d7afb4851f661f5
Xyzor <ricsibi23@gmail.com>
2024-10-14 22:04:48 +0200

    refactor

--- index.js
+++ index.js
@@ -7,75 +7,76 @@ const fgXOffsetInput = document.getElementById('fgXOffset');
 const fgYOffsetInput = document.getElementById('fgYOffset');
 const overwriteOnlyNonZero = document.getElementById('overwriteOnlyNonZero');
 const downloadCanvasLink = document.getElementById('downloadCanvas');
-const uploadStatusArea = document.getElementById('uploadStatusArea');
 const previewArea = document.getElementById('previewArea');
 const reader = new FileReader();
 let imagesInPixel = [];
 
 const compositeAOverB = (aColor, aAlpha, bColor, bAlpha, combinedAlpha) => {
     if (combinedAlpha === 0) return bColor;
+
     return (aAlpha * aColor + ((1 - aAlpha) * bAlpha * bColor)) / combinedAlpha;
 }
 
-const blendForegroundIntoBackground = (bgImg, fgImg, fgAlpha, bgAlpha, xOffsetOfFgInPixel, yOffsetOfFgInPixel) => {
-    if (fgAlpha === 0 && bgAlpha === 0) return bgImg;
+const blendForegroundIntoBackground = (imgBg, imgFg, alphaFg, alphaBg, xOffsetInPixelFg = 0, yOffsetInPixelFg = 0) => {
+    if (alphaFg === 0 && alphaBg === 0) return imgBg;
 
-    // const combinedAlpha = fgAlpha + ((1 - fgAlpha) * bgAlpha);
     const bytesPerPixel = 4;
-    const sizeOfImgInBytesFg = fgImg.width * fgImg.height * bytesPerPixel;
-    const xOffsetOfFgInBytes = (xOffsetOfFgInPixel * bytesPerPixel);
-    const yOffsetOfFgInBytes = (yOffsetOfFgInPixel * bgImg.width * bytesPerPixel);
-    const offsetInPixel = (yOffsetOfFgInPixel * bgImg.width) + xOffsetOfFgInPixel;
+    const sizeOfImgInBytesFg = imgFg.width * imgFg.height * bytesPerPixel;
+    const xOffsetInBytesFg = (xOffsetInPixelFg * bytesPerPixel);
+    const yOffsetInBytesFg = (yOffsetInPixelFg * imgBg.width * bytesPerPixel);
 
-    for (let byteIndex = 0; byteIndex < sizeOfImgInBytesFg; byteIndex+= bytesPerPixel) {
-        const redIndexFg = byteIndex;
-        const greenIndexFg = byteIndex + 1;
-        const blueIndexFg = byteIndex + 2;
-        const alphaIndexFg = byteIndex + 3;
-        const redIndexBg = yOffsetOfFgInBytes + xOffsetOfFgInBytes + redIndexFg;
+    for (let byteIndexFg = 0; byteIndexFg < sizeOfImgInBytesFg; byteIndexFg += bytesPerPixel) {
+        const redIndexFg = byteIndexFg;
+        const greenIndexFg = byteIndexFg + 1;
+        const blueIndexFg = byteIndexFg + 2;
+        const alphaIndexFg = byteIndexFg + 3;
+        const redIndexBg = yOffsetInBytesFg + xOffsetInBytesFg + redIndexFg;
         const greenIndexBg = redIndexBg + 1;
         const blueIndexBg = redIndexBg + 2;
         const alphaIndexBg = redIndexBg + 3;
-        
-        if (overwriteOnlyNonZero.checked && !fgImg.data[alphaIndexFg]) continue;
 
-        const alphaPercentageFg = typeof fgAlpha === 'number' ? fgAlpha : fgImg.data[alphaIndexFg] / 255;
-        const alphaPercentageBg = typeof bgAlpha === 'number' ? bgAlpha : bgImg.data[alphaIndexBg] / 255;
-        const combinedAlpha = alphaPercentageFg + ((1 - alphaPercentageFg) * alphaPercentageBg);
+        const alphaInPercentageFg = typeof alphaFg === 'number' ? alphaFg : imgFg.data[alphaIndexFg] / 255;
+        const alphaInPercentageBg = typeof alphaBg === 'number' ? alphaBg : imgBg.data[alphaIndexBg] / 255;
+        const combinedAlpha = alphaInPercentageFg + ((1 - alphaInPercentageFg) * alphaInPercentageBg);
 
+        // Debugging values
+        /*
         const currentPixelFg = (redIndexFg / bytesPerPixel) + 1;
         const currentPixelBg = (redIndexBg / bytesPerPixel) + 1;
-        const currentRowFg = Math.floor((redIndexFg / bytesPerPixel) / fgImg.width) + 1;
-        const currentRowBg = Math.floor((redIndexBg / bytesPerPixel)  / bgImg.width) + 1;
-        const currentColumnFg = (currentPixelFg % fgImg.width) || fgImg.width;
-        const currentColumnBg = (currentPixelBg % bgImg.width) || bgImg.width;
-        const isRowOverflow = (currentRowBg - yOffsetOfFgInPixel) != currentRowFg;
-        const pixelOfFgIsOutsideOfBg = isRowOverflow || bgImg.data[redIndexBg] === undefined;
+        const currentColumnFg = (currentPixelFg % imgFg.width) || imgFg.width;
+        const currentColumnBg = (currentPixelBg % imgBg.width) || imgBg.width;
+        */
 
-        if (pixelOfFgIsOutsideOfBg) continue;
+        const rowOfCurrentPixelFg = Math.floor((redIndexFg / bytesPerPixel) / imgFg.width) + 1;
+        const rowOfCurrentPixelBg = Math.floor((redIndexBg / bytesPerPixel)  / imgBg.width) + 1;
+        const isRowOverflow = (rowOfCurrentPixelBg - yOffsetInPixelFg) != rowOfCurrentPixelFg;
+        const fgPixelIsOutsideOfBg = isRowOverflow || imgBg.data[redIndexBg] === undefined;
+        const ignoreTransparentPixelFg = imgFg.data[alphaIndexFg] === 0 && overwriteOnlyNonZero.checked;
 
-        bgImg.data[redIndexBg] =
+        if (ignoreTransparentPixelFg || fgPixelIsOutsideOfBg) continue;
+
+        imgBg.data[redIndexBg] =
             compositeAOverB(
-                fgImg.data[redIndexFg],
-                alphaPercentageFg,
-                bgImg.data[redIndexBg],
-                alphaPercentageBg,
+                imgFg.data[redIndexFg],
+                alphaInPercentageFg,
+                imgBg.data[redIndexBg],
+                alphaInPercentageBg,
                 combinedAlpha
             );
-        bgImg.data[greenIndexBg] =
+        imgBg.data[greenIndexBg] =
             compositeAOverB(
-                fgImg.data[greenIndexFg],
-                alphaPercentageFg,
-                bgImg.data[greenIndexBg],
-                alphaPercentageBg,
+                imgFg.data[greenIndexFg],
+                alphaInPercentageFg,
+                imgBg.data[greenIndexBg],
+                alphaInPercentageBg,
                 combinedAlpha
             );
-        bgImg.data[blueIndexBg] =
+        imgBg.data[blueIndexBg] =
             compositeAOverB(
-                fgImg.data[blueIndexFg],
-                alphaPercentageFg,
-                bgImg.data[blueIndexBg],
-                alphaPercentageBg,
+                imgFg.data[blueIndexFg],
+                alphaInPercentageFg,
+                imgBg.data[blueIndexBg],
+                alphaInPercentageBg,
                 combinedAlpha
             );
     }
@@ -121,28 +122,24 @@ const refreshImagesInPixelArray = () => {
 }
 
 const handleFileReaderEvent = (event) => {
-    uploadStatusArea.innerHTML += `<span>${event.type}: ${event.loaded} bytes transferred</span><br/>`;
+    const imageElement = document.createElement('img');
+    imageElement.src = reader.result; // image data in base64
+    imageElement.classList = 'previewImg';
+    imageElement.title = fileInput.files[0].name;
+    imageElement.onload = () => {
+        previewArea.append(imageElement);
 
-    if (event.type === "load") {
-        const imageElement = document.createElement('img');
-        imageElement.src = reader.result; // image data in base64
-        imageElement.classList = 'previewImg';
-        imageElement.title = fileInput.files[0].name;
-        imageElement.onload = () => {
-            previewArea.append(imageElement);
-
-            // Re-generate pixel-info array for every image so that on new upload,
-            // the blending doesn't use the color values from previous blending,
-            // resulting in extra blendings.
-            // It's due to the fact that it's using mutation on the imagesInPixel global array elements.
-            //
-            // I don't like this global-array-mutation approach
-            // but it's the easiest and there's no other reason to change.
-            refreshImagesInPixelArray();
-            drawBlendedImageDataOnCanvas();
-            updateImageDownloadLink();
-        };
-    }
+        // Re-generate pixel-info array for every image so that on new upload,
+        // the blending doesn't use the color values from previous blending,
+        // resulting in extra blendings.
+        // It's due to the fact that it's using mutation on the imagesInPixel global array elements.
+        //
+        // I don't like this global-array-mutation approach
+        // but it's the easiest and there's no other reason to change.
+        refreshImagesInPixelArray();
+        drawBlendedImageDataOnCanvas();
+        updateImageDownloadLink();
+    };
 }
 
 const updateImageDownloadLink = () => {
@@ -152,15 +149,7 @@ const updateImageDownloadLink = () => {
 
 fileInput.addEventListener('change', () => {
 	if (fileInput.files[0]) {
-        uploadStatusArea.innerHTML += `<b>${fileInput.files[0].name}</b><hr/>`;
-
-        reader.addEventListener("loadstart", handleFileReaderEvent);
         reader.addEventListener("load", handleFileReaderEvent);
-        reader.addEventListener("loadend", handleFileReaderEvent);
-        reader.addEventListener("progress", handleFileReaderEvent);
-        reader.addEventListener("error", handleFileReaderEvent);
-        reader.addEventListener("abort", handleFileReaderEvent);
-
         reader.readAsDataURL(fileInput.files[0]);
 	}
 });
